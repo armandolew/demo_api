74a36c55bd5765a391762efcfb87651ff9c01129
Armando Lew <alew16@hotmail.com>
2017-12-25 22:36:28 -0600

    Fixing error messages in registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,35 +1,37 @@
 class RegistrationsController < ApplicationController
 
   def sign_up
-    user = User.create(email: params[:data][:attributes][:email], password: params[:data][:attributes][:password])
-
-    if user.valid?
-      #UserNotificationsMailer.sign_up_mailer(user).deliver
+    begin
+      user = User.create(email: params[:data][:attributes][:email], password: params[:data][:attributes][:password])
       render json: render_element_json(user, UserResource)
-    else
-      render json: "#{user.errors}".to_json
+    rescue => e
+      handle_exceptions(e)
     end
   end
 
   def sign_in
-    user = User.find_by(email: params[:data][:attributes][:email])
+  	begin
+      user = User.find_by(email: params[:data][:attributes][:email])
     
-    if user && user.authenticate(params[:data][:attributes][:password]) 
-      render json: render_element_json(user, UserResource)
-    else
-      render json: "Not found".to_json
-    end
+      if user && user.authenticate(params[:data][:attributes][:password]) 
+        render json: render_element_json(user, UserResource)
+      end
+    rescue => e
+      handle_exceptions(e)
+    end    
   end
 
   def confirmation
-  	user = User.find_by(confirmation_token: params[:confirmation_token])
+  	begin
+  	  user = User.find_by(confirmation_token: params[:confirmation_token])
 
-  	if user && user.active == false
-  	  user.update(active: true)
-  	  render json: render_element_json(user, UserResource)
-  	else
-  	  render json: "Not found".to_json
-    end 
+  	  if user && user.active == false
+  	    user.update(active: true)
+  	    render json: render_element_json(user, UserResource)
+      end
+    rescue => e
+      handle_exceptions(e)
+    end  
   end
 
 end
